b65443a1369e28b3676ce1283354a9c4fb4d5be2
Kyle Halterman <halterman.kyle@gmail.com>
2015-02-11 23:05:45 -0800

    wrapped the entire mail script in an isset

--- process-contact.php
+++ process-contact.php
@@ -2,26 +2,27 @@
 
 session_start();
 
-// Diskin@TrueNWVentures.com
-
-
-$to="halterman.kyle@gmail.com";
-
-$name=$_POST['name'];
-$from=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
-
-$headers = "This message is from $name email address: $from";
 
 if (isset($_POST['submit']))
 {
-	$_SESSION['success'] = "Your email has been sent, we will be in contact with you soon, thank you!";
-	
-	mail($to, $subject, $message, $headers);
-	
-	header('location: contact.php');
-	die();
+
+	// Diskin@TrueNWVentures.com
+
+	$to="halterman.kyle@gmail.com";
+
+	$name=$_POST['name'];
+	$from=$_POST['email'];
+	$subject=$_POST['subject'];
+	$message=$_POST['message'];
+
+	$headers = "This message is from $name email address: $from";
+
+		$_SESSION['success'] = "Your email has been sent, we will be in contact with you soon, thank you!";
+		
+		mail($to, $subject, $message, $headers);
+		
+		header('location: contact.php');
+		die();
 }
 
 ?>
